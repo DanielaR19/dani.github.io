c3041a0bd3bc49c212d82a91782b92d371375a9e
Daniela Rivera <>
2025-06-18 13:35:32 -0600

    arreglo del boton cerrar del modal intento 6

--- main.js
+++ main.js
@@ -545,7 +545,6 @@ function createModalTools() {
     const DIV_MODAL_HEADER_BTN = document.createElement('button');
     DIV_MODAL_HEADER_BTN.setAttribute('type', 'button');
     DIV_MODAL_HEADER_BTN.className = 'close';
-    DIV_MODAL_HEADER_BTN.setAttribute('data-dismiss', 'modal');
     DIV_MODAL_HEADER_BTN.setAttribute('data-bs-dismiss', 'modal');
     DIV_MODAL_HEADER.appendChild(DIV_MODAL_HEADER_BTN);
 
